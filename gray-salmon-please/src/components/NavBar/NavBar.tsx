835ac325910aa007859646d3399753dd7db2f124
sungyotkim <sungyotkim@gmail.com>
2022-11-20 12:19:48 -0500

    add responsiveness to navbar

--- gray-salmon-please/src/components/NavBar/NavBar.tsx
+++ gray-salmon-please/src/components/NavBar/NavBar.tsx
@@ -1,47 +1,98 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./NavBar.css";
 import { SiGmail, SiGithub, SiLinkedin, SiAngellist } from "react-icons/si";
+import { GiHamburgerMenu } from "react-icons/gi";
+import websiteLogo from "../../assets/website-logo-tommy.png";
 
 // interface NavBarProps {}
 
+const getWindowSize = () => {
+  const { innerWidth } = window;
+  return innerWidth;
+};
+
 export const NavBar: React.FC<{}> = ({}) => {
+  const [windowSize, setWindowSize] = useState<number | (() => number)>(
+    getWindowSize()
+  );
+  const [showMobileDropdown, setShowMobileDropdown] = useState<boolean>(true);
+
+  const handleClick = () => {
+    setShowMobileDropdown(!showMobileDropdown);
+  };
+
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowSize());
+    }
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (windowSize > 500) {
+      setShowMobileDropdown(true);
+    } else {
+      setShowMobileDropdown(false);
+    }
+  }, [windowSize]);
+
   return (
     <div className="nav-wrapper">
       <div className="nav-container">
         <div className="nav-name-container">Gray Salmon Please!</div>
-        <div className="nav-links">
-          <div
-            onClick={() =>
-              (window.location.href = "mailto:sungyotkim@gmail.com")
-            }
-            className="nav-link"
-          >
-            <SiGmail />
+        <div className="mobile-nav-link-wrapper">
+          <div className="mobile-burger-menu" onClick={handleClick}>
+            <GiHamburgerMenu />
           </div>
-          <a
-            href="https://github.com/sungyotkim"
-            target={"_blank"}
-            rel="noreferrer"
-            className="nav-link"
-          >
-            <SiGithub />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/sungyotkim/"
-            target={"_blank"}
-            rel="noreferrer"
-            className="nav-link"
-          >
-            <SiLinkedin />
-          </a>
-          <a
-            href="https://angel.co/u/sungyo-tommy-kim"
-            target={"_blank"}
-            rel="noreferrer"
-            className="nav-link"
-          >
-            <SiAngellist />
-          </a>
+          {showMobileDropdown && (
+            <div className="nav-links">
+              <div
+                onClick={() =>
+                  (window.location.href = "mailto:sungyotkim@gmail.com")
+                }
+                className="nav-link"
+              >
+                <SiGmail />
+              </div>
+              <a
+                href="https://github.com/sungyotkim"
+                target={"_blank"}
+                rel="noreferrer"
+                className="nav-link"
+              >
+                <SiGithub />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/sungyotkim/"
+                target={"_blank"}
+                rel="noreferrer"
+                className="nav-link"
+              >
+                <SiLinkedin />
+              </a>
+              <a
+                href="https://angel.co/u/sungyo-tommy-kim"
+                target={"_blank"}
+                rel="noreferrer"
+                className="nav-link"
+              >
+                <SiAngellist />
+              </a>
+              <a
+                href="https://tommykim.me"
+                target={"_blank"}
+                rel="noreferrer"
+                className="nav-link"
+              >
+                <img src={websiteLogo} alt="portfolio" />
+              </a>
+            </div>
+          )}
         </div>
       </div>
     </div>
