a2c5e9ae6e9059d9d32e46b28f8647b9827afd3b
sungyotkim <sungyotkim@gmail.com>
2022-11-28 20:45:20 -0500

    add introduction

--- gray-salmon-please/src/components/MainContentBody/Introduction/Introduction.tsx
+++ gray-salmon-please/src/components/MainContentBody/Introduction/Introduction.tsx
@@ -1,12 +1,26 @@
 import "./Introduction.css";
 import React from "react";
+import salmonFillet from "../../../assets/salmon-fillet.jpeg";
 
 interface IntroductionProps {}
 
 export const Introduction: React.FC<IntroductionProps> = ({}) => {
   return (
     <>
-      <div></div>
+      <div className="introduction-container">
+        <div className="introduction-text-container">
+          <div className="introduction-header">Did you know?</div>
+          <div>
+            Farmed salmon is naturally <span className="bold-text">gray!</span>
+            <br />
+            The classic <span className="salmon-text">salmon color</span> is
+            only natural to <span className="salmon-text">wild salmon</span>.
+          </div>
+        </div>
+        <div className="introduction-hero-container">
+          <img src={salmonFillet} alt="salmon-fillet" id="introduction-hero" />
+        </div>
+      </div>
     </>
   );
 };
--- gray-salmon-please/src/components/MainContentBody/MainContentBody.tsx
+++ gray-salmon-please/src/components/MainContentBody/MainContentBody.tsx
@@ -1,13 +1,15 @@
-import "./MainContentBody.css";
-
 import React from "react";
+import "./MainContentBody.css";
+import { Introduction } from "./Introduction/Introduction";
 
 interface MainContentBodyProps {}
 
 export const MainContentBody: React.FC<MainContentBodyProps> = ({}) => {
   return (
     <>
-      <div>hi</div>
+      <div className="main-content-wrapper">
+        <Introduction />
+      </div>
     </>
   );
 };
