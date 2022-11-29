acd884503e1cbbe8f8d2c03384079a74cd70cf78
sungyotkim <sungyotkim@gmail.com>
2022-11-28 22:03:34 -0500

    add styling to comparison

--- gray-salmon-please/src/components/MainContentBody/Comparison/Comparison.tsx
+++ gray-salmon-please/src/components/MainContentBody/Comparison/Comparison.tsx
@@ -8,13 +8,14 @@ export const Comparison: React.FC<ComparisonProps> = ({}) => {
     <>
       <div className="comparison-container">
         <h2>Farmed vs Wild</h2>
-        <h2>What's the difference?</h2>
+        <h3>What's the difference?</h3>
         <div className="comparison-table">
           <div className="comparison-left-container">
             <li>Generally lower price</li>
             <li>Available year round</li>
             <li>Longer shelf life</li>
             <li>Less diverse species options</li>
+            <li>Requires artificial coloring</li>
           </div>
           <div className="comparison-divider"></div>
           <div className="comparison-right-container">
@@ -22,6 +23,7 @@ export const Comparison: React.FC<ComparisonProps> = ({}) => {
             <li>Available seasonally (spring and summer)</li>
             <li>Shorter shelf life</li>
             <li>Greater species variety during the season</li>
+            <li>Naturally colored</li>
           </div>
         </div>
       </div>
