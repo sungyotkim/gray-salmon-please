077e7f5d5d0a8c31367a944a2cd70b241eb903b4
sungyotkim <sungyotkim@gmail.com>
2022-12-05 14:56:49 -0500

    add styling

--- gray-salmon-please/src/components/InteractiveCharts/BarChartInfo/BarChartInfo.tsx
+++ gray-salmon-please/src/components/InteractiveCharts/BarChartInfo/BarChartInfo.tsx
@@ -1,16 +1,21 @@
 import React from "react";
+import "./BarChartInfo.css";
 
 interface BarChartInfoProps {}
 
 export const BarChartInfo: React.FC<BarChartInfoProps> = ({}) => {
   return (
     <>
-      <div>
+      <div className="bar-chart-info-container">
         The following chart displays consumers' estimated willingness to pay (in
-        USD) per kg of salmon. The results are split into two parts. The first
-        set of data,<span className="bold-text"> before information</span>,
-        represents consumers' initial willingness to pay for a kg of salmon of
-        the specified color. The second set of data,
+        USD) per kg of salmon. The results are split into two parts.
+        <br />
+        The first set of data,
+        <span className="bold-text"> before information</span>, represents
+        consumers' initial willingness to pay for a kg of salmon of the
+        specified color.
+        <br />
+        The second set of data,
         <span className="bold-text"> after information</span>, represents
         consumers' willingness to pay for a kg of salmon of the specified color
         after receiving information about the origin of the salmon's color. The
