235a5f017580e98e92b3a96af9a5880cc9a765b0
sungyotkim <sungyotkim@gmail.com>
2022-12-06 20:20:36 -0500

    add title and final styling to table

--- gray-salmon-please/src/components/InteractiveSlider/InteractiveSlider.tsx
+++ gray-salmon-please/src/components/InteractiveSlider/InteractiveSlider.tsx
@@ -1,19 +1,30 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./InteractiveSlider.css";
 import { SliderDescription } from "./SliderDescription/SliderDescription";
 import { SliderImage } from "./SliderImage/SliderImage";
 import { SliderComponent } from "./SliderComponent/SliderComponent";
 import { SliderTable } from "./SliderTable/SliderTable";
+import { DarkModeContext } from "../../context/DarkModeContext";
 
 interface InteractiveSliderProps {}
 
 export const InteractiveSlider: React.FC<InteractiveSliderProps> = ({}) => {
   const [sliderColor, setSliderColor] = useState<string>("#ecb092");
   const [sliderCost, setSliderCost] = useState<number>(20);
+  const { darkMode } = useContext(DarkModeContext);
 
   return (
     <>
       <SliderDescription />
+      <div
+        className={
+          darkMode
+            ? "interactive-slider-title"
+            : "interactive-slider-title interactive-slider-title-light"
+        }
+      >
+        Salmon Color Price Estimator
+      </div>
       <div className="interactive-slider-container">
         <div className="interactive-slider-left">
           <SliderImage sliderColor={sliderColor} />
@@ -25,7 +36,7 @@ export const InteractiveSlider: React.FC<InteractiveSliderProps> = ({}) => {
           />
         </div>
         <div className="interactive-slider-right">
-          <SliderTable />
+          <SliderTable sliderCost={sliderCost} />
         </div>
       </div>
     </>
--- gray-salmon-please/src/components/InteractiveSlider/SliderTable/SliderTable.tsx
+++ gray-salmon-please/src/components/InteractiveSlider/SliderTable/SliderTable.tsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import "./SliderTable.css";
+import { sliderCosts } from "../../../utils/sliderData";
+import { DarkModeContext } from "../../../context/DarkModeContext";
 
-interface SliderTableProps {}
+interface SliderTableProps {
+  sliderCost: number;
+}
 
-export const SliderTable: React.FC<SliderTableProps> = ({}) => {
+export const SliderTable: React.FC<SliderTableProps> = ({ sliderCost }) => {
+  const { darkMode } = useContext(DarkModeContext);
+  const sliderTableElement = document.getElementById("slider-table");
+
+  useEffect(() => {
+    if (darkMode && sliderTableElement) {
+      sliderTableElement.style.setProperty("--tableColor", "#767c7c");
+    }
+  }, [darkMode]);
+
   return (
     <>
-      <div className="slider-table-wrapper"></div>
+      <div
+        className={
+          darkMode
+            ? "slider-table-wrapper slider-table-text-dark"
+            : "slider-table-wrapper"
+        }
+        id="slider-table"
+      >
+        <div className="slider-table-title">Estimated market price</div>
+        <div className="slider-table-main bold-text">
+          ${sliderCosts[sliderCost - 20]}
+        </div>
+        <div className="slider-table-footer">per lb of salmon</div>
+      </div>
     </>
   );
 };
